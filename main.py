42a5457b062692463ff34587c7562d2a13098995
Audrey Andoy <audreylandoy@gmail.com>
2020-09-11 23:58:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Project planning
+# 2 players: player vs. computer
+# 1) ask player for their choice
+# either chooses from rock paper or scissors
+# 2) computer randomly makes choice
+# 3) printing both inputs
+# 4) Outputting/Comparing who won:
+# paper beats rock
+# scissors beats paper
+# rock beats scissors
+
+print("***WELCOME TO ROCK PAPER SCISSORS SHOWDOWN***")
